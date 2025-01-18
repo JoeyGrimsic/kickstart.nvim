5fbbdccbcf8f5c0fa221435f28090b39200266c6
JoeyGrimsic <golfpro4034@gmail.com>
2025-01-18 14:16:24 -0500

    Added themery and clipboard config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,33 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Themery.nvim
+  {
+    'zaldih/themery.nvim',
+    lazy = false,
+    config = function()
+      require('themery').setup {
+        themes = { 'gruvbox', 'tokyonight', 'monokai', 'citruszest', 'ashen' },
+        livePreview = true,
+      }
+    end,
+  },
+
+  -- Example themes
+  { 'morhetz/gruvbox' }, -- Gruvbox
+  --{ 'ayu-theme/ayu-vim' }, -- Ayu
+  { 'folke/tokyonight.nvim' }, -- Tokyo Night
+  { 'tanvirtin/monokai.nvim' },
+  { 'zootedb0t/citruszest.nvim' },
+  --{ 'scottmckendry/cyberdream.git' },
+  { 'folke/tokyonight.nvim' },
+  --{ 'sainnhe/gruvbox-material.git' },
+  --{ 'sainnhe/everforest.git' },
+  { 'ficcdaf/ashen.nvim' },
+
+  {
+    'goolord/alpha-nvim',
+    dependencies = { 'echasnovski/mini.icons' },
+    config = function()
+      require('alpha').setup(require('alha.themes.startify').config)
+    end,
+  },
+}
