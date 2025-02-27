7531facb7f76d934787b8cced15b6da25e3c059b
JoeyGrimsic <golfpro4034@gmail.com>
2025-02-27 18:45:52 -0500

    added transparency toggle

--- init.lua
+++ init.lua
@@ -1,61 +1,5 @@
 --[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
 Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
   Next, run AND READ `:help`.
     This will open up a help window with some basic information
     about reading, navigating and searching the builtin help documentation.
@@ -70,23 +14,15 @@ Kickstart Guide:
     These are hints about where to find more information about the relevant settings,
     plugins or Neovim features used in Kickstart.
 
-   NOTE: Look for lines like this
-
     Throughout the file. These are for you, the reader, to help you understand what is happening.
     Feel free to delete them once you know what you're doing, but they should serve as a guide
     for when you are first encountering a few different constructs in your Neovim config.
 
 If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
 
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
--- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+--  ]]
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
@@ -98,10 +34,7 @@ vim.g.have_nerd_font = false
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
 vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
@@ -976,6 +909,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   { import = 'custom.plugins' },
+
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,50 +1,87 @@
 return {
-  -- Themery
+  -- Themery configuration
   {
     'zaldih/themery.nvim',
     lazy = false,
     config = function()
       require('themery').setup {
-        themes = { 'gruvbox', 'tokyonight', 'monokai', 'citruszest', 'ashen' },
+        themes = { 'gruvbox', 'tokyonight', 'monokai', 'citruszest', 'ashen', 'gruvbox-material' },
         livePreview = true,
+        beforeReload = function(theme)
+          _G.force_transparency()
+        end,
       }
     end,
   },
-  -- Colorbuddy
+
+  -- Universal transparency setup with toggle
   {
-    'tjdevries/colorbuddy.nvim',
+    'nvim-lua/plenary.nvim',
+    config = function()
+      -- State management
+      _G.transparency_enabled = true
+
+      -- Transparency function
+      _G.force_transparency = function()
+        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'CursorLine', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'CursorLineNr', { bg = 'none' })
+        vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+      end
+
+      -- Toggle function
+      _G.toggle_transparency = function()
+        _G.transparency_enabled = not _G.transparency_enabled
+        if _G.transparency_enabled then
+          _G.force_transparency()
+          vim.notify('Transparency enabled', vim.log.levels.INFO)
+        else
+          -- Reset to theme defaults
+          vim.cmd('colorscheme ' .. vim.g.colors_name)
+          vim.notify('Transparency disabled', vim.log.levels.INFO)
+        end
+      end
+
+      -- Initial application
+      vim.schedule(_G.force_transparency)
+
+      -- Theme change handler
+      vim.api.nvim_create_autocmd('ColorScheme', {
+        pattern = '*',
+        callback = function()
+          if _G.transparency_enabled then
+            _G.force_transparency()
+          end
+        end,
+      })
+
+      -- Keymap
+      vim.keymap.set('n', '<leader>tt', '<cmd>lua toggle_transparency()<CR>', {
+        desc = '[T]oggle [T]ransparency',
+        silent = true,
+      })
+    end,
   },
 
-  -- THEMES LIST
-  { 'morhetz/gruvbox' }, -- Gruvbox
-  --{ 'ayu-theme/ayu-vim' }, -- Ayu
-  { 'folke/tokyonight.nvim' }, -- Tokyo Night
+  -- Theme plugins
+  { 'folke/tokyonight.nvim' },
+  { 'morhetz/gruvbox' },
   { 'tanvirtin/monokai.nvim' },
   { 'zootedb0t/citruszest.nvim' },
-  --{ 'scottmckendry/cyberdream.git' },
-  --{ 'sainnhe/gruvbox-material.git' },
-  --{ 'sainnhe/everforest.git' },
   { 'ficcdaf/ashen.nvim' },
-
-  -- Alpha: Nvim Cover Page
-  --  {
-  --    'goolord/alpha-nvim',
-  --    dependencies = { 'echasnovski/mini.icons' },
-  --    config = function()
-  --      require('alpha').setup(require('alpha.themes.startify').config)
-  --    end,
-  --  },
-
-  -- Gruvbox-material
   {
     'sainnhe/gruvbox-material',
     lazy = false,
     priority = 1000,
     config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
       vim.g.gruvbox_material_enable_italic = true
-      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
+
+  -- Other plugins
+  { 'tjdevries/colorbuddy.nvim' },
+  { 'ThePrimeagen/vim-be-good' },
 }
